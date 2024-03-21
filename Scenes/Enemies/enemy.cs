ff3b94d0f3fb44cbf5bdb20ccfd93de89354d144
halhel1 <halhel1@lsu.edu>
2024-03-21 14:16:15 -0500

    .

--- Scenes/Enemies/enemy.cs
+++ Scenes/Enemies/enemy.cs
@@ -4,41 +4,83 @@ using Godot;
 
 public partial class enemy : CharacterBody2D
 {
-	private int speed = 100;
-	private Node player = null;
-	private bool playerChase = true;
-	private AnimatedSprite2D enemySprite;
-	private RayCast2D enemyLOS;
-	public override void _Ready()
+   
+    private int speed = 100;
+    private Node player = null;
+    private bool playerChase = true;
+
+    private Vector2 velocity = Vector2.Zero;
+
+    private AnimatedSprite2D enemySprite;
+
+    
+
+    public enemy()
+    {
+        
+    }
+
+    public override void _Ready()
+    {
+        enemySprite=GetNode<Godot.AnimatedSprite2D>("enemySprite");
+         enemySprite.Play("idle");
+       
+        
+
+    }
+    public override void _Process(double delta)
 	{
-		enemySprite=GetNode<Godot.AnimatedSprite2D>("enemySprite");
-		enemySprite.Play("idle");
-		enemyLOS=GetNode<RayCast2D>("enemyLOS");
-	}
-	public override void _Process(double delta)
-	{
-		
-	}
-	   public override void _PhysicsProcess(double delta){
-			if (playerChase && player != null) {
-			if (player is Area2D kinematicPlayer)
-			{
-			Vector2 direction = (kinematicPlayer.GlobalPosition -GlobalPosition).Normalized();
-			GlobalPosition += direction * speed * (float)delta;   
-			}
-		}
+        
 	}
 
-	private void _on_detection_area_area_entered(Node body){
-		player=body;
-		playerChase=true;
-		enemySprite.Stop();
-	}
+    
 
-	private void _on_detection_area_area_exited(Node body){
-		player=null;
-		playerChase=false;
-		enemySprite.Play("idle");
-	}
+    
+   
+       public override void _PhysicsProcess(double delta){
+    
+        
+             if (playerChase && player != null) {
+        
+        
+            
+            if (player is Area2D kinematicPlayer)
+            {
+              
+
+                 Vector2 direction = (kinematicPlayer.GlobalPosition -GlobalPosition).Normalized();
+                GlobalPosition += direction * speed * (float)delta;
+               
+            
+                
+            }
+             
+        }
+    
+    
+
+    }
+
+    private void _on_detection_area_area_entered(Node body){
+        
+
+
+        player=body;
+        playerChase=true;
+        enemySprite.Stop();
+        
+        
+
+    }
+
+    private void _on_detection_area_area_exited(Node body){
+       
+
+        player=null;
+        playerChase=false;
+        enemySprite.Play("idle");
+        
+
+    }
 
 }
