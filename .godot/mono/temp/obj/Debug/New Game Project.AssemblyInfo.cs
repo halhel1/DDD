fd7a3c41db27aaca2a755d3da5ec127845843b1d
halhel1 <halhel1@lsu.edu>
2024-04-19 17:31:14 -0500

    .

--- .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aedee9cfab9a98bd866e64782d83b73d94f26ee3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51be49075c64cdc45264455a5e1cf03b806d7e00")]
 [assembly: System.Reflection.AssemblyProductAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
