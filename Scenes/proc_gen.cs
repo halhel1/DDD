25350013094a523583486ff65e1ab81302a5e2e5
halhel1 <halhel1@lsu.edu>
2024-04-28 12:18:25 -0500

    added camera bounds and border(hardcoded) and added stretch

--- Scenes/proc_gen.cs
+++ Scenes/proc_gen.cs
@@ -5,7 +5,7 @@ using System.Linq;
 public partial class proc_gen : Node2D
 { 
     [Export]
-    public NoiseTexture2D noiseHeightText;
+    public NoiseTexture2D noiseHeightTeXt;
     private FastNoiseLite noise;
     private float[] noiseValArr;
     private int width = 50;
@@ -22,6 +22,7 @@ public partial class proc_gen : Node2D
     int ground2=2;
     int ground3=3;
     int ground4=4;
+    PackedScene Border;
     public override void _Ready()
     {
         tileMap = GetNode<TileMap>("TileMap");
@@ -34,33 +35,47 @@ public partial class proc_gen : Node2D
     void GenerateLevel()
     {
         noiseValArr = new float[(width) * (height)];
-     
-        for (int x = 1; x < width-1; x++)
+        for (int X = 0; X < width; X++)
+    {
+        for (int Y = 0; Y < height; Y++)
         {
-            for (int y = 1; y < height-1; y++)
+            if (X == 0 || X == width - 1 || Y == 0 || Y == height - 1)
             {
-                float noiseVal = noise.GetNoise(x, y);
-                noiseValArr[x + y * width] = noiseVal;
+                
+                tileMap.SetCell(ground3, new Vector2I(X, Y), 0, borderAtlas, 0);
+                
+                
+            }
+        }
+    }
+     
+        for (int X = 1; X < width-1; X++)
+        {
+            for (int Y = 1; Y < height-1; Y++)
+            {
+                float noiseVal = noise.GetNoise(X, Y);
+                noiseValArr[X + Y * width] = noiseVal;
                 if(noiseVal >= 0.0){
-                    tileMap.SetCell(ground,new Vector2I(x,y),0,dirtAtlas,0);
+                    tileMap.SetCell(ground,new Vector2I(X,Y),0,dirtAtlas,0);
                     if (rand.Next(3) == 0){
-                    tileMap.SetCell(ground1, new Vector2I(x, y), 0, seaweedAtlas, 0);
+                    tileMap.SetCell(ground1, new Vector2I(X, Y), 0, seaweedAtlas, 0);
                     }
                 }
                  else if(noiseVal >= -0.5){
-                    tileMap.SetCell(ground1,new Vector2I(x,y),0,grassAtlas,0);
+                    tileMap.SetCell(ground1,new Vector2I(X,Y),0,grassAtlas,0);
                     if (rand.Next(3) == 0){
-                    tileMap.SetCell(ground1, new Vector2I(x, y), 0, dirtAtlas, 0);
+                    tileMap.SetCell(ground1, new Vector2I(X, Y), 0, dirtAtlas, 0);
                     }
                 }
                 else {
-                    tileMap.SetCell(ground2,new Vector2I(x,y),0,dirtAtlas,0);
+                    tileMap.SetCell(ground2,new Vector2I(X,Y),0,dirtAtlas,0);
                     if (rand.Next(9) == 0){
-                    tileMap.SetCell(ground2, new Vector2I(x, y), 0, grassAtlas, 0);
+                    tileMap.SetCell(ground2, new Vector2I(X, Y), 0, grassAtlas, 0);
                     } 
                 }
             
             }
         }
     }
+   
 }
