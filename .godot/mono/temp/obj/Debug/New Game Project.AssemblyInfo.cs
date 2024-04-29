8aed58205afcaf14abbec11e5a53c46c03d23de6
柒Yue_N <jniu0514@gmail.com>
2024-04-29 02:43:05 -0500

    fix bug

--- .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c53f18a895fbd9de85870a5536d1b88ac61cbbec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24d33703fc8d8927a2620a1dcd94af885bbc364c")]
 [assembly: System.Reflection.AssemblyProductAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
