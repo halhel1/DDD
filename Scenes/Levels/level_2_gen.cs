925bf6c4b28119ab088e56313a4d853ee50ba2c1
halhel1 <halhel1@lsu.edu>
2024-04-29 01:16:10 -0500

    border hotfix

--- Scenes/Levels/level_2_gen.cs
+++ Scenes/Levels/level_2_gen.cs
@@ -4,13 +4,13 @@ using System;
 public partial class level_2_gen : proc_gen
 {
 	private Vector2I stoneAtlas= new Vector2I(3,1);
-    private Vector2I seaweedAtlas=new Vector2I(3,4);
+    private Vector2I flowerAtlas=new Vector2I(3,4);
     private Vector2I grassAtlas=new Vector2I(4,1);
 
     public override void _Ready()
     {
         base._Ready();
-        GenerateLevel(50,100, stoneAtlas, seaweedAtlas, grassAtlas);
+        GenerateLevel(50,100, stoneAtlas, flowerAtlas, grassAtlas);
     }
 
     public override void _Process(double delta)
