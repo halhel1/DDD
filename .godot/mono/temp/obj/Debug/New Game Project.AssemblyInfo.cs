53c09fdd96d87f8b44af6e46813c1fb068b16712
halhel1 <halhel1@lsu.edu>
2024-04-29 22:16:34 -0500

    player not updating between levels need to fix

--- .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8e6daf5eba97ae944e4e549286f585ed602621b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+429f3d15b9a3e643dd36f098ae3ad3741f3f2a24")]
 [assembly: System.Reflection.AssemblyProductAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
