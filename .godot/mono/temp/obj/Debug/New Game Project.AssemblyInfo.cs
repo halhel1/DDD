5639d6e298c0ab36a0800c5ea1b1021868ada5cb
halhel1 <halhel1@lsu.edu>
2024-04-30 12:23:57 -0500

    removed old options

--- .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51261b53b008bb8ffa051af43f315b4912bb540c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fde8d8cbb19da46bc33ab4f084f398a51940113")]
 [assembly: System.Reflection.AssemblyProductAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
