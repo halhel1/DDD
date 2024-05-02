d6013b459d58554e2ff650c97a24c43d8ba69291
halhel1 <halhel1@lsu.edu>
2024-05-02 02:15:54 -0500

    added next level and levels loop after

---
+++ Scenes/Levels/level_3_gen.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+public partial class level_3_gen : proc_gen
+
+{
+    private Vector2I stoneAtlas = new Vector2I(6, 5);
+    private Vector2I flowerAtlas = new Vector2I(3, 4);
+    
+
+    int width, height;
+
+    public override void _Ready()
+    {
+        base._Ready(); 
+
+        
+        width = 50; 
+        height = 100; 
+
+        
+        GenerateLevel(width, height, stoneAtlas, flowerAtlas, stoneAtlas);
+    }
+
+    public override void _Process(double delta)
+    {
+        // Process logic
+    }
+}
--- Scenes/cave.gd
+++ Scenes/cave.gd
@@ -2,7 +2,10 @@
 extends Area2D
 func _on_area_entered(area: Area2D):
 	if area.is_in_group("player"):
-		var current_scene_file = get_tree().current_scene.scene_file_path
-		var next_lvl_num = current_scene_file.to_int() + 1
-		var next_lvl_path = "res://Scenes/Levels/level_" + str(next_lvl_num) + ".tscn"
-		get_tree().change_scene_to_file(next_lvl_path)
+		if(get_tree().current_scene.scene_file_path.to_int() != 3):
+			var next_lvl_num = get_tree().current_scene.scene_file_path.to_int() + 1
+			var next_lvl_path = "res://Scenes/Levels/level_" + str(next_lvl_num) + ".tscn"
+			print(next_lvl_path)
+			get_tree().change_scene_to_file(next_lvl_path)
+		else:
+			get_tree().change_scene_to_file("res://Scenes/Levels/level_1.tscn")
