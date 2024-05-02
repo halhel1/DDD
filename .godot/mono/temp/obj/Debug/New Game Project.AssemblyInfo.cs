fed953bb51fece666ad1c7f9fe7f424ff0cf3e74
thomshim <tshime1@lsu.edu>
2024-05-02 16:49:55 -0500

    Improved weapon spread, Upscaled projectile
     Improved cooldown bar logic

--- .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46ba2f77b1df24dbb0d830fc2df7bc4f0001098b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6013b459d58554e2ff650c97a24c43d8ba69291")]
 [assembly: System.Reflection.AssemblyProductAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Scenes/Weapons/Types/shotgun.gd
+++ Scenes/Weapons/Types/shotgun.gd
@@ -1,21 +1,18 @@
 class_name Shotgun extends "res://Scenes/Weapons/weapons_base.gd"
 
-var rng = RandomNumberGenerator.new()
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	speed = 600
-	scale = Vector2(1,1)
-	damage_amount = 5
-	$WeaponCooldown.wait_time = cooldown
-	pass # Replace with function body.
+	scale_vector = Vector2(0.5,0.5)
+	damage_amount = 2.5
+	spread_radius = 20
+	$WeaponCooldown.wait_time = 1
 
-func fire() -> void:
-	for x in 20:
-		var projectile = projectile_scene.instantiate()
-		projectile.direction = get_global_mouse_position() - global_position 
-		projectile.direction += Vector2(rng.randf_range(-80,80),rng.randf_range(-80,80))
-		projectile.global_position = global_position
-		projectile.speed = speed
-		projectile.damage_amount = damage_amount
-		get_tree().get_root().add_child(projectile)
-	sfx_shoot.play()
+
+func _process(_delta):
+	if(Input.is_action_pressed("attack")&&can_fire):
+		for x in 50:
+			fire()
+		sfx_shoot.play()
+		can_fire = false
+		$WeaponCooldown.start()
