dc013e25038a93814a10977d81dfe857f47a47ad
halhel1 <halhel1@lsu.edu>
2024-05-04 15:54:23 -0500

    camera bounds

--- .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a800aa7a662067dbe806d44654d101763ace4fff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4f801d5cfa9bbe4f5c605494839c2b255fffb82")]
 [assembly: System.Reflection.AssemblyProductAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
