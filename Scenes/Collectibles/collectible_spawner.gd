67692efbc6113c34fef20039d5d7db4e12c79296
halhel1 <halhel1@lsu.edu>
2024-05-08 22:43:29 -0500

    added missing type hinting for gdscript

--- Scenes/Collectibles/collectible_spawner.gd
+++ Scenes/Collectibles/collectible_spawner.gd
@@ -2,19 +2,19 @@ extends spawner
 const GameManager = preload("res://Scenes/Levels/level_manager.gd")
 var entitySpawns: int = 2;
 
-var colors = [
+var colors: Array = [
 	Color(0, 1, 0),
 	Color(0, 1, 1),
 	Color(0, 0.5, 1)
 ]
 
 func _ready():
-	entitySpawns+=LevelManager.get_current_level()*2
+	entitySpawns+=LevelManager.get_current_level()
 	super._ready()
 	set_entity(load("res://Scenes/Collectibles/heart.tscn"), 200)
 	spawn_entities(entitySpawns)
 	set_entity(load("res://Scenes/Collectibles/seashell.tscn"), 200)
 	spawn_entities(entitySpawns)
 
-func is_valid_entity(new_pos, effective_radius):
+func is_valid_entity(new_pos: Vector2, effective_radius: float):
 	return super.is_valid_entity(new_pos, effective_radius)
--- Scenes/Player/cam.gd
+++ Scenes/Player/cam.gd
@@ -6,18 +6,20 @@ func _ready() -> void:
 	setCam()
 	
 
-	pass # Replace with function body.
+	pass 
 
 func setCam():
-	var map_width_pixels = 0
-	var map_height_pixels = 0 
+	var limit_top
+	var map_width_pixels: int  = 0
+	var map_height_pixels: int = 0 
 	if LevelManager.get_current_level()%3==0:
-		map_width_pixels = 110 * 81 
+		map_width_pixels  = 110 * 81 
 		map_height_pixels = 55 * 40 
 		limit_right = map_width_pixels
 		limit_top = 55
 		limit_left=135
 		limit_bottom = map_height_pixels
+		#all base members of base Camera
 		
 		pass
 	elif LevelManager.get_current_level()%3==2:
@@ -27,6 +29,7 @@ func setCam():
 		limit_top = 55
 		limit_left=135
 		limit_bottom = map_height_pixels
+		#all base members of base Camera
 		pass
 	else:
 		map_width_pixels = 55 * 80 
@@ -35,5 +38,6 @@ func setCam():
 		limit_top = 55
 		limit_left=135
 		limit_bottom = map_height_pixels
+		#all base members of base Camera
 		pass
 	
