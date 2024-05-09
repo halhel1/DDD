da306d1f2fe729fb8129ea9641d426e2c827ff97
halhel1 <halhel1@lsu.edu>
2024-05-09 00:16:51 -0500

    fixing dependencies

--- Assets/.godot/mono/temp/obj/Debug/Menu.AssemblyInfo.cs
+++ Assets/.godot/mono/temp/obj/Debug/Menu.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Menu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c9e9fed429ac4fb55e291db6014ec08dd07314f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c84e17413bdbe165d28f350abcb025f1cb5a8f9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Menu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Menu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
